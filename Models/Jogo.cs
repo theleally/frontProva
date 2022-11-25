5beae94876aba74a64a957eef8dae04ee14696c6
Leandro Allysson <64550528+theleally@users.noreply.github.com>
2022-11-25 02:00:02 -0300

    key

--- Models/Jogo.cs
+++ Models/Jogo.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace API_Copa.Models
 {
@@ -10,6 +11,8 @@ namespace API_Copa.Models
             SelecaoB = new Selecao();
             CriadoEm = DateTime.Now;
         }
+
+        [Key]
         public int Id { get; set; }
         public Selecao SelecaoA { get; set; }
         public Selecao SelecaoB { get; set; }
